378269eadefed147a20b0b0b40eb7ce7b8a11bdd
jamezjaz <jamezjaz@gmail.com>
2020-12-04 15:11:11 -0600

    docs: adds more tests to reverseString function

--- reverseString.test.js
+++ reverseString.test.js
@@ -3,3 +3,11 @@ const reverseString = require('./reverseString');
 test('first letter becomes last and the last becomes first', () => {
   expect(reverseString('James')).toBe('semaJ');
 });
+
+test('reverses a string', () => {
+  expect(reverseString('hello')).not.toBe('oLlEh');
+});
+
+test('should fail when a string isn\t reversed', () => {
+  expect(reverseString('hello')).not.toBe('hello');
+});
